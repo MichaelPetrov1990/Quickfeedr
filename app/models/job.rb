5001f68880709dad4d46bcb5d11ed6a153bc71d8
Henry Martin <henry.martin01@hotmail.com>
2020-08-25 16:13:30 +0100

    picture cards

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,9 @@
 class Job < ApplicationRecord
   belongs_to :user
+  # include PgSearch::Model
+  # pg_search_scope :search_by_title_and_location,
+  #   against: [:title, :location],
+  #     using: {
+  #       tsearch: { prefix: true }
+  #             }
 end
