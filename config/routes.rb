b4b6de5c3579ad67e0aeba6e031267773d8dae29
Henry Martin <henry.martin01@hotmail.com>
2020-08-25 17:13:44 +0100

    search bar now works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/dashboard', to: 'pages#recruitment_dashboard'
+  get '/dashboard', to: 'pages#recruitment_dashboard', as: "dashboard"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :jobs
