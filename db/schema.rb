79c957850831fc5fd105795336a9c5706f082ac5
Henry Martin <henry.martin01@hotmail.com>
2020-08-25 12:44:53 +0100

    added the user profile pic to the nav-bar and set up cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_08_24_155511) do
-
-
+ActiveRecord::Schema.define(version: 2020_08_25_095715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "job_applications", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -27,8 +44,6 @@ ActiveRecord::Schema.define(version: 2020_08_24_155511) do
     t.index ["job_id"], name: "index_job_applications_on_job_id"
     t.index ["user_id"], name: "index_job_applications_on_user_id"
   end
-
-
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -54,7 +69,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_155511) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "job_applications", "jobs"
   add_foreign_key "job_applications", "users"
   add_foreign_key "jobs", "users"
