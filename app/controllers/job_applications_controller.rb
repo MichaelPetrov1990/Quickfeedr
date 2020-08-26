853cac5b7b7cf8069badedd40c3f5b9adbe38990
Henry Martin <henry.martin01@hotmail.com>
2020-08-26 12:16:34 +0100

    working on application controller

---
+++ app/controllers/job_applications_controller.rb
@@ -0,0 +1,43 @@
+class JobApplicationsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = current_user
+    @job = Job.find(params[:job_id])
+    @job_application = JobApplication.new
+  end
+
+  def create
+    @job = Job.find(params[:job_id])
+    @job_application = JobApplication.new
+    @job_application.job = @job
+    @job_application.user = current_user
+    if @job_application.save!
+      redirect_to jobs_path
+    else
+      flash[]
+    end
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def job_params
+
+  end
+end
