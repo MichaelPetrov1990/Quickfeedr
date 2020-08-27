3c6029113e534e125b3624f0d1ea4acc76493abd
Henry Martin <henry.martin01@hotmail.com>
2020-08-27 16:16:02 +0100

    fixed map zoom

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -62,6 +62,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :location, :salary, :description)
+    params.require(:job).permit(:title, :location, :salary, :description, :job_spec)
   end
 end
