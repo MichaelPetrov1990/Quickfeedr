530803c73bda32a78e0005065ea3facb328a7c7c
Henry Martin <henry.martin01@hotmail.com>
2020-08-27 15:25:14 +0100

    JS map on job index page now works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 // import { displayShow } from "../components/job_show.js"
 import { initMapbox } from '../plugins/init_mapbox';
+import { memorizeScroll} from '../components/memorize_scroll'
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   // displayShow();
   initMapbox();
+  memorizeScroll();
 });
 
