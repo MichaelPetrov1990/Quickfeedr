39a66f849d75f5792b0ac8e67e276b3ece71acf8
Henry Martin <henry.martin01@hotmail.com>
2020-09-01 14:29:23 +0100

    added feedback to interview index

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -17,6 +17,4 @@ class FeedbacksController < ApplicationController
     redirect_to job_job_application_interviews_path(@jobs, @job_application, @feedback.inteview)
   end
 end
-
-
-job_job_application_interviews GET    /jobs/:job_id/job_applications/:job_application_id/interviews(.:format)                  interviews#index
+               interviews#index
