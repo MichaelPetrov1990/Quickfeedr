1afd4916f39f320c0719db7e6019a74485727ffe
Henry Martin <henry.martin01@hotmail.com>
2020-09-02 12:01:43 +0100

    styling applicant profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
   has_one_attached :cv
-
   has_many :jobs
+  has_many :job_applications
 
   def fullname
     "#{first_name} #{last_name}"
